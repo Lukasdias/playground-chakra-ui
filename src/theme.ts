e9f29323b3583fd78269bc1a2ddd641d8fb59525
Lukas Henrique Dias <diaslukas19@gmail.com>
2022-05-18 15:28:01 -0300

    feat: initial commit

--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,20 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  fonts: {
+    heading: 'Poppins, sans-serif',
+    body: 'Poppins, sans-serif',
+    mono: 'Poppins, sans-serif',
+  },
+  colors: {
+    transparent: 'transparent',
+    test_salmon: '#F9A88C',
+    test_purple: '#734CA2',
+    test_pink: '#f48190',
+    test_green: '#44d8b7',
+  },
+});
 
 export default theme;
